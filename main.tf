2eb000cc9565b83e87aa4c4ac8bd9fee7e605d01
Lucas0Cezario <oliveiralucas03@gmail.com>
2023-06-22 12:00:37 -0300

    TERRAFORM VM AWS 7

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
     bucket = "lucas0cezarioteste"
-    key    = "aws-pipeline-github-actions/terraform.tfstate"
+    key    = "aws-vpc/terraform.tfstate"
     region = "us-west-2"
   }
 }
