303ab533622b26fa5bb0a6e99408e80b5ed7f695
Lucas0Cezario <oliveiralucas03@gmail.com>
2023-06-22 13:50:05 -0300

    TERRAFORM VM AWS 10

--- main.tf
+++ main.tf
@@ -1,12 +1,22 @@
 terraform {
+
   required_version = ">= 1.0.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "5.4.0"
     }
   }
+
+  backend "s3" {
+    bucket = "lucas0cezarioteste"
+    key    = "aws-vm-Pipeline/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
+
+
 
 provider "aws" {
   region = "us-west-2"
@@ -19,11 +29,10 @@ provider "aws" {
   }
 }
 
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-  config = {
-    bucket = "lucas0cezarioteste"
-    key    = "aws-vpc/terraform.tfstate"
-    region = "us-west-2"
-  }
+module "network" {
+  source = "./network"
+
+  cidr_vpc    = "10.0.0.0/16"
+  cidr_subnet = "10.0.1.0/24"
+  environment = "desenvolvimento"
 }
--- vm-aws.tf
+++ vm-aws.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "vm" {
   ami                         = "ami-03f65b8614a860c29"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
-  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  subnet_id                   = module.network.security_group_id
+  vpc_security_group_ids      = [module.network.security_group_id]
   associate_public_ip_address = true
 
   tags = {
