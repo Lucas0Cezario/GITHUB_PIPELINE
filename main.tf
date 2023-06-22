194a6c5c4640b3d1d77efc016548ec8a27d50b91
Lucas0Cezario <oliveiralucas03@gmail.com>
2023-06-22 10:41:34 -0300

    TERRAFORM VM AWS 2

--- main.tf
+++ main.tf
@@ -1,12 +1,18 @@
 terraform {
-
   required_version = ">= 1.0.0"
-
+  required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.73.0"
+      version = "5.4.0"
     }
   }
+  backend "s3" {
+    bucket = "lucas0cezarioteste"
+    key    = "aws-PIPE-GIT/terraform.tfstate"
+    region = "us-west-2"
+
+  }
+}
 
 provider "aws" {
   region = "us-west-2"
