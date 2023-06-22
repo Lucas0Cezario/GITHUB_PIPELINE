6714d2d302986ae2f22a4d2712e8ddb4ae55af8d
Lucas0Cezario <oliveiralucas03@gmail.com>
2023-06-22 14:13:25 -0300

    TERRAFORM VM AWS 11

--- vm-aws.tf
+++ vm-aws.tf
@@ -7,7 +7,7 @@ resource "aws_instance" "vm" {
   ami                         = "ami-03f65b8614a860c29"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = module.network.security_group_id
+  subnet_id                   = module.network.subnet_id
   vpc_security_group_ids      = [module.network.security_group_id]
   associate_public_ip_address = true
 
