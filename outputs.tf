5740b76514cd9deaa15ff3d5e350c63b789cb6e2
Lucas0Cezario <oliveiralucas03@gmail.com>
2023-06-22 12:15:39 -0300

    TERRAFORM VM AWS 8

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "vm_aws_ip" {
   value = aws_instance.vm.public_ip
 }
+
+output "subnet_id" {
+  value = aws_subnet.subnet_id
+  description = "The ID of the subnet"
+}
+
+output "security_group_id" {
+  value = aws_security_group.security_group_id
+  description = "The ID of the security group"
+}
